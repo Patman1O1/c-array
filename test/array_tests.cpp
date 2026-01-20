142a0cae9ed5b10e8c64892218de321f4f6dbdcc
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-20 12:35:04 -0600

    Edit array_tests.cpp

--- test/array_tests.cpp
+++ test/array_tests.cpp
@@ -3,3 +3,16 @@
 
 #include <array/array.h>
 
+namespace array_testing {
+
+    /* ----------------------------------------------------array_init------------------------------------------------ */
+    TEST(array_init_suite, zero_size) {
+        struct array array = {};
+        array_init(int, array, 0);
+
+        EXPECT_EQ(array.values, nullptr);
+        EXPECT_EQ(array.size, 0);
+    }
+
+
+} // namespace array_testing
