19476f14cd42b3ce0669bc1aa00f700ca1531c56
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-20 01:52:22 -0600

    Edit array.h

---
+++ include/array/array.h
@@ -0,0 +1,12 @@
+#ifndef ARRAY_H
+#define ARRAY_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // #ifdef __cplusplus
+
+#ifdef __cplusplus
+}
+#endif // #ifdef __cplusplus
+
+#endif // #ifndef ARRAY_H
