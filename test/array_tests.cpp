6125d9d85b571c60b0d921779173ebd99ee5bab4
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-20 20:41:35 -0600

    Edit array_tests.cpp

--- test/array_tests.cpp
+++ test/array_tests.cpp
@@ -1,3 +1,5 @@
+#include <array>
+
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
@@ -14,7 +16,7 @@ namespace array_testing {
         EXPECT_EQ(array.values, nullptr);
         EXPECT_EQ(array.size, 0);
 
-        array_free(&array);
+        EXPECT_EQ(array_free(&array), EXIT_SUCCESS);
     }
 
     TEST(array_init_suite, num_values_greater_than_array_size) {
@@ -26,9 +28,23 @@ namespace array_testing {
         EXPECT_EQ(array.values, nullptr);
         EXPECT_EQ(array.size, 1);
 
-        array_free(&array);
+        EXPECT_EQ(array_free(&array), EXIT_SUCCESS);
     }
 
+    TEST(array_init_suite, num_values_equals_array_size) {
+        struct array array = {};
 
+        array_init(int, array, 3, 1, 2, 3);
+
+        EXPECT_NE(array.values, nullptr);
+        EXPECT_EQ(array.size, 3);
+
+        for (std::size_t i = 0; i < array.size; i++) {
+            constexpr std::array matcher = {1, 2, 3};
+            EXPECT_EQ(array_at(int, array, i), matcher[i]);
+        }
+
+        EXPECT_EQ(array_free(&array), EXIT_SUCCESS);
+    }
 
 } // namespace array_testing
