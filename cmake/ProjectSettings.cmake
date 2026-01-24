b26583380097e67a074bdf45235ed63a858263d0
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-23 23:58:46 -0600

    Edit ProjectSettings.cmake

---
+++ cmake/ProjectSettings.cmake
@@ -0,0 +1,11 @@
+#-----------------------------------------------------------------------------------------------------------------------
+# Settings
+#-----------------------------------------------------------------------------------------------------------------------
+# C Settings
+set(CMAKE_C_STANDARD 23)
+set(CMAKE_C_STANDARD_REQUIRED ON)
+
+# Project Settings
+option(PROJECT_BUILD_TESTS "Build the library tests" OFF)
+option(PROJECT_BUILD_SHARED_LIBS "Build the library as a shared library" OFF)
+
